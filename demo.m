fb063f136885600c882f084140146f6817363ecc
HYOT Lucas <lucash21200303@gmail.com>
2024-05-17 15:19:46 +0200

    ajout du code pour calculer l'histogramme

--- demo.m
+++ demo.m
@@ -1,38 +1,44 @@
-
-import numpy as np
-
-from matplotlib import pyplot as plt
-import cv2 as cv
-
-if __name__ == "__main__":
+% Demo Lecture et Affichage Image Couleur
+%
+clear all; close all;
+%
 
 
-    #Ouvrir une image
-    img = cv.imread("CerisierP.jpg")
-    img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
 
-    #Afficher une image
-    plt.figure()
-    plt.imshow(img)
+%  lecture et affichage d'une image:
+nom=input('Entrer le nom image avec le format jpg, bmp ou tif :','s'); % l'image doit être dans le répertoire
+a=imread(nom);
 
-    #Afficher les canaux
-    rouge = img[:,:, 0]
-    vert = img[:,:, 1]
-    bleu = img[:,:, 2]
-    plt.figure()
-    plt.subplot(2, 2, 1)
-    plt.imshow(img)
-    plt.subplot(2, 2, 2)
-    plt.imshow(rouge, cmap="Reds")
-    plt.subplot(2, 2, 3)
-    plt.imshow(vert, cmap="Greens")
-    plt.subplot(2, 2, 4)
-    plt.imshow(bleu, cmap="Blues")
+%{
+figure(1);
+subplot(2,2,1);
+imagesc(a);
 
-    # Transformation en niveau de gris
-    imgG = cv.cvtColor(img,cv.COLOR_RGB2GRAY)
-    plt.figure()
-    plt.imshow(imgG, cmap="gray")
+% transformation image en niveau de gris:
+rouge=double(a(:,:,1));
+vert=double(a(:,:,2));
+bleu=double(a(:,:,3));
+subplot(2,2,2);
+imagesc(rouge, [min(min(rouge)) max(max(rouge))]);
+subplot(2,2,3);
+imagesc(vert, [min(min(vert)) max(max(vert))]);
+subplot(2,2,4);
+imagesc(bleu, [min(min(bleu)) max(max(bleu))]);
+colormap(gray);
+%}
+%
+% Transformation image couleur en une image niveaux de gris:
+%
+image=double(rgb2gray(a));
+figure(2);
+imagesc(image, [min(min(image)) max(max(image))]);
+colormap(gray);
 
-    plt.show()
+
+
+histogramme(image);
+
+
+
+
 
---
+++ histogramme.m
@@ -0,0 +1,26 @@
+function histogramme(image)
+
+  taille = size(image);
+
+  width = taille(2)
+  height = taille(1);
+
+  valeurs = zeros(1,256);
+
+
+  for w = 1:width
+    for h = 1:height
+
+      pixel = image(h,w);
+
+      valeurs(pixel+1)= valeurs(pixel+1)+1;
+
+
+
+    endfor
+  endfor
+
+  figure(3);
+  plot(valeurs);
+
+  endfunction
